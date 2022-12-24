2b253b0a07ea80ca4c49b6a055ca19af3c422701
Ayush Yadav <ayushyadav935@gmail.com>
2022-12-24 14:20:47 +0530

    image bug

--- Server/index.js
+++ Server/index.js
@@ -10,8 +10,9 @@ require('./config/auth/passportJWT');
 
 const port = process.env.PORT || 5000
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: "30mb",extended:true}));
 app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use('/', require('./api/routes'));
